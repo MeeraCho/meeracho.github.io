1f23399654b063ce8f556d56fbe8572e3845157b
MiraCho <miracho119@gmail.com>
2024-06-16 14:34:33 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `meeracho.github.io`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
